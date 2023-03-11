6213d8e5d996204bb791ab5c621aa7110b593e57
Olame Muliri <92609361+Oam21689@users.noreply.github.com>
2023-03-11 03:27:58 -0600

    Initial Commit

---
+++ GShotDetect/GShotDetectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GShotDetectApp.swift
+//  GShotDetect
+//
+//  Created by Olame Muliri on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GShotDetectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
